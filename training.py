e8f8a80f17e05ab1e19868d58d4b25aee40e3853
wjm55 <william.mattingly@yale.edu>
2025-03-26 15:00:50 -0300

    Remove device parameter from model training call in training.py

--- training.py
+++ training.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
 
 # Train the model
-results = model.train(data="data.yaml", epochs=100, imgsz=640, device=[0,1])
+results = model.train(data="data.yaml", epochs=100, imgsz=640)
